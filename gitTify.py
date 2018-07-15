ba778efc52c6061b319bbd4bab7bd2fae6dafaa3
pedro <pofc24@gmail.com>
2018-07-15 22:38:06 +0100

    Code more organized, windows support(Not tested), beautifull code!

--- gitTify.py
+++ gitTify.py
@@ -1,85 +1,29 @@
 # -*- coding: utf-8 -*-
 
 #Imports necessários !
-import thread
-import threading
 import time
 import sys
-import pynotify
-import urllib2
-import xml.etree.ElementTree as ET
-from win10toast import ToastNotifier #pip install win10toast
+import os
 
+try:
+    import linux.gitTify as linux
+except ImportError:
+    print "Linux notifications not supported"
+    print "Please install punotify"
 
-#Array Global de controlo de Hora de update para cada thread
-lastReadUpdate = []
+try:
 
-
-def notifyWindows(str,rep):
-    toaster.show_toast(rep,str, icon_path=None,duration=5,threaded=True)
-    # Wait for threaded notification to finish
-    while toaster.notification_active(): time.sleep(0.1)
-
-def notifyLinux(str,rep):
-    pynotify.init("gitTify")
-    notice = pynotify.Notification(rep, str)
-    notice.show()
-    time.sleep(5)
-    notice.close()
-    return
-
-def getFeed(rep,branch,idx):
-    rep=rep+"/commits/"+branch+".atom"
-    while 1:
-            file = urllib2.urlopen(rep)
-            data = file.read()
-            root = ET.fromstring(data)
-            entry=root.find('{http://www.w3.org/2005/Atom}entry')
-            id = entry.find('{http://www.w3.org/2005/Atom}id').text
-            title = entry.find('{http://www.w3.org/2005/Atom}title').text
-            timeUpdate = entry.find('{http://www.w3.org/2005/Atom}updated').text
-            name = entry.find('{http://www.w3.org/2005/Atom}author')
-            name = name.find('{http://www.w3.org/2005/Atom}name').text
-            global lastReadUpdate
-            if lastReadUpdate[idx]!=timeUpdate:
-                lastReadUpdate[idx]=timeUpdate
-                if timeUpdate == getLastUpdate(rep):
-                    str ="New Commit on "+branch+ " - from: "+name +";\nTitle:"+title+";\nID:"+id+"; \nTime:"+timeUpdate
-                    if sys.platform.startswith('linux'):
-                        notifyLinux(str,rep)
-                    elif sys.platform.startswith('win'):
-                        notifyWindows(str,rep)
-                    time.sleep(200)
-                else:
-                    time.sleep(50)
-            else:
-                time.sleep(100)
-            file.close()
-
-
-
-def getLastUpdate(rep):
-    file = urllib2.urlopen(rep)
-    data = file.read()
-    root = ET.fromstring(data)
-    lastUpdate= root.find('{http://www.w3.org/2005/Atom}updated').text
-    return lastUpdate
+    import windows10.gitTify as windows10
+except ImportError:
+    print "windows notifications not supported"
+    print "Please install win10toast"
 
 
 def main(argv):
-    rep=argv[1];
-    sizearg=len(argv)
-    global lastReadUpdate
-    lastReadUpdate.append("")
-    lastReadUpdate.append("")
-    try:
-        for  x in range(2,sizearg):
-            lastReadUpdate.append("")
-            thread.start_new_thread( getFeed, (rep,argv[x],x))
-
-    except:
-        print "Error: unable to start thread"
-
+    if sys.platform.startswith('linux'):
+        linux.start(argv)
+    elif sys.platform.startswith('win'):
+        windows10.start(argv)
 
     while 1:
         pass
---
+++ linux/gitTify.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+#Imports necessários !
+import thread
+import threading
+import time
+import sys
+import pynotify
+import urllib2
+import xml.etree.ElementTree as ET
+
+
+#Array Global de controlo de Hora de update para cada thread
+lastReadUpdate = []
+
+
+
+def notifyLinux(str,rep):
+    pynotify.init("gitTify")
+    notice = pynotify.Notification(rep, str)
+    notice.show()
+    time.sleep(5)
+    notice.close()
+    return
+
+def getFeed(rep,branch,idx):
+    rep=rep+"/commits/"+branch+".atom"
+    while 1:
+            file = urllib2.urlopen(rep)
+            data = file.read()
+            root = ET.fromstring(data)
+            entry=root.find('{http://www.w3.org/2005/Atom}entry')
+            id = entry.find('{http://www.w3.org/2005/Atom}id').text
+            title = entry.find('{http://www.w3.org/2005/Atom}title').text
+            timeUpdate = entry.find('{http://www.w3.org/2005/Atom}updated').text
+            name = entry.find('{http://www.w3.org/2005/Atom}author')
+            name = name.find('{http://www.w3.org/2005/Atom}name').text
+            global lastReadUpdate
+            if lastReadUpdate[idx]!=timeUpdate:
+                lastReadUpdate[idx]=timeUpdate
+                if timeUpdate == getLastUpdate(rep):
+                    str ="New Commit on "+branch+ " - from: "+name +";\nTitle:"+title+";\nID:"+id+"; \nTime:"+timeUpdate
+                    notifyLinux(str,rep)
+                    time.sleep(200)
+                else:
+                    time.sleep(50)
+            else:
+                time.sleep(100)
+            file.close()
+
+
+
+def getLastUpdate(rep):
+    file = urllib2.urlopen(rep)
+    data = file.read()
+    root = ET.fromstring(data)
+    lastUpdate= root.find('{http://www.w3.org/2005/Atom}updated').text
+    return lastUpdate
+
+
+def start(argv):
+    rep=argv[1]
+    sizearg=len(argv)
+    global lastReadUpdate
+    lastReadUpdate.append("")
+    lastReadUpdate.append("")
+    try:
+        for x in range(2,sizearg):
+            lastReadUpdate.append("")
+            thread.start_new_thread( getFeed, (rep,argv[x],x))
+
+    except:
+        print "Error: unable to start thread"
